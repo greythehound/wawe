51ad6d9460667a232540f6bab3f4e9fc17b76616
Sergey Kizenko <sergey.kizenko@walkme.com>
2020-11-06 23:31:41 +0200

    header fixes, header adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const menuBtn = document.querySelector('.menu__btn');
+  const menuList = document.querySelector('.menu__list');
+
+  menuBtn.addEventListener('click', function () {
+    menuList.classList.toggle('menu__list--active');
+  });
+
+});
+
+
+
+
 $(function () {
   var mixer = mixitup('.gallery__content');
 
